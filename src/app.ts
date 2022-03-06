636378fac33277fd81ff9a2e838f0cb01e053733
ys2486 <yusaku19940728@yahoo.co.jp>
2022-03-06 20:02:33 +0900

    typescript google-map-app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,47 @@
-import { ProjectInput } from './components/project-input';
-import { ProjectList } from './components/project-list';
+import axios from '../node_modules/axios/index';
 
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+//Google APIにて
+const GOOGLE_API_KEY = 'AIzaSyDdnlzBuE2g6hzbAKke9yg7PlEH_D8i6e4';
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
+declare var google: any;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== 'OK') {
+        throw new Error('座標を取得できませんでした');
+      }
+      console.log(response);
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: coordinates,
+        zoom: 16,
+      });
+      new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener('submit', searchAddressHandler);
